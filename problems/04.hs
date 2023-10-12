8edf6f9b3037ef9653d1897e854f135b85d29a7d
simsax <simsax98@gmail.com>
2023-10-12 16:54:38 +0200

    tail call optimization for problem 4

--- problems/04.hs
+++ problems/04.hs
@@ -1,3 +1,9 @@
 myLength :: [a] -> Int
 myLength [] = 0
 myLength (_:xs) = 1 + myLength xs
+
+myLength' :: [a] -> Int
+myLength' xs = myLengthTail xs 0
+    where
+        myLengthTail [] acc = acc
+        myLengthTail (_:xs) acc = myLengthTail xs (acc + 1)
